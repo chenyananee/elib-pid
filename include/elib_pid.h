5fdc51ccf5f5a87626e24dcec01ba55a1cbab15d
yanan.chen <yanan.chen8@gientech.com>
2026-05-12 14:45:44 +0800

    feat: add umbrella header

---
+++ include/elib_pid.h
@@ -0,0 +1,9 @@
+/* elib_pid.h - PID Controller Main Header */
+#ifndef ELIB_PID_H
+#define ELIB_PID_H
+
+#include "elib_pid_defs.h"
+#include "elib_pid_pos.h"
+#include "elib_pid_inc.h"
+
+#endif /* ELIB_PID_H */
